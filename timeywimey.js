42b6390d0adea36ba1b5c335a9d9dc9aa2441369
Skyler Brungardt <skyler.brungardt@gmail.com>
2013-07-19 19:55:39 +0200

    Added inline docs, user state listening, cleanup.
    Functions now all return things nicely, along with resetting the idle
    count when the user does something.

--- timeywimey.js
+++ timeywimey.js
@@ -1,20 +1,114 @@
-// Todo: 
-// 2. Tracking for user input!
+/*
+"timeywimey" is a task scheduler and idle-detector for JavaScript, 
+by Skyler Brungardt and David Powell.
+Copyright (C) 2013  Skyler Brungardt
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Adds TimeyWimey to the global scope, exposed as "TW".
+ * @param  {Object} root The global object, usually 'window', 'module.exports', 
+ *                       or similar.
+ * @return {Boolean}      Returns true when initialized.
+ */
 !(function setupTimeyWimey (root) {
 
+  /**
+   * Constructor.  Adds event listeners to catch for user input, and initializes
+   * timer for idle detection.
+   */
   function TimeyWimey () {
+
+    /**
+     * Resets the idle counter when user input is detected.
+     * @return {Number} Returns the reset counter.
+     */
+    function userInput () {
+      count = 0;
+
+      return count;
+    }
     
+    var i;
     var count = 0;
     var _this = this;
+    var events = [
+      'blur',
+      'click',
+      'contextmenu',
+      'dblclick',
+      'devicelight',
+      'devicemotion',
+      'deviceorientation',
+      'deviceproximity',
+      'drag',
+      'dragend',
+      'dragenter',
+      'dragleave',
+      'dragover',
+      'dragstart',
+      'drop',
+      'focus',
+      'fullscreenchange',
+      'keydown',
+      'keypress',
+      'keyup',
+      'mousedown',
+      'mouseenter',
+      'mouseleave',
+      'mousemove',
+      'mouseout',
+      'mouseover',
+      'mouseup',
+      'resize',
+      'scroll',
+      'select',
+      'show',
+      'submit',
+      'touchend',
+      'touchenter',
+      'touchleave',
+      'touchmove',
+      'touchstart',
+      'wheel'
+    ];
     
+    /**
+     * Initializes a timeout to track the delay between when it's called, and 
+     * when it actually initializes.  There's a fuzziness which is expected, but
+     * anything longer than the fuzziness means the JS engine is working on
+     * something else, and thus isn't idle.
+     * 
+     * @return {Number} Returns the timeout id.
+     */
     var checkIdle = function checkIdle () {
       var lastCalled = Date.now();
+      var fuzziness = 2;
 
-      setTimeout(function idle () {
+      /**
+       * Timeout tracks the difference between when it was called last, and when
+       * it executes.  Determines an idle state by speed at which it gets called
+       * over a short period of time, and when idle, executes any queued tasks.
+       * 
+       * @return {Function} Recursive; retruns checkIdle function.
+       */
+      return setTimeout(function idle () {
         var now = Date.now();
         var gap = now - lastCalled;
 
-        if (gap > (_this.tick + 2)) {
+        if (gap > (_this.tick + fuzziness)) {
 
           count = 0;
 
@@ -34,7 +128,7 @@
           _this.executeIdleTasks();
         }
 
-        checkIdle();
+        return checkIdle();
       }, _this.tick);
     };
 
@@ -43,9 +137,26 @@
     this.tasks = {};
     this.defaultInterval = 100;
 
+    for (i = 0; i < events.length; i++) {
+
+      document.addEventListener(events[i], userInput);
+
+    }
+
     checkIdle();
+
   }
 
+  /**
+   * Queues a task to be executed after a given delay.  
+   * Similar to pubsub pattern.
+   * 
+   * @param  {String}   label    Label identifying the tasks to execute.
+   * @param  {Function} callback The task to execute.
+   * @param  {Boolean}   queue    Whether to queue the task with others, or make
+   *                              it a single, standalone task.
+   * @return {Object}            Returns the labeled object.
+   */
   TimeyWimey.prototype.queueTask = function queueTask (label, callback, queue) {
     var _this = this;
     queue = queue || false;
@@ -82,6 +193,12 @@
 
   };
 
+  /**
+   * Immediately executes any tasks queued for a given label.
+   * @param  {String} label The label for which tasks will be executed.
+   * @return {Boolean}       Returns true if tasks were executed, false if none
+   *                         existed for that label.
+   */
   TimeyWimey.prototype.executeTasks = function executeTasks (label) {
     var i;
     var _callbacks;
@@ -121,6 +238,11 @@
 
   };
 
+  /**
+   * Executes the next task in any given queue, removing it from the queue.
+   * @param  {String} label The label corresponding to the desired queue.
+   * @return {Function}       Returns the task executed.
+   */
   TimeyWimey.prototype.nextTask = function nextTask (label) {
 
     if (this.tasks[label].callbacks) {
@@ -131,6 +253,12 @@
 
   };
 
+  /**
+   * Executes the most recently added task to any queue, removing it from the
+   * queue.
+   * @param  {String} label The label corresponding to the desired queue.
+   * @return {Function}       Returns the task executed.
+   */
   TimeyWimey.prototype.lastTask = function lastTask (label) {
 
     if (this.tasks[label].callbacks) {
@@ -141,6 +269,11 @@
 
   };
 
+  /**
+   * Executes any and all tasks currently queued.
+   * @return {Boolean} Returns true after triggering executeTasks() method for
+   *                   each queue.
+   */
   TimeyWimey.prototype.executeIdleTasks = function executeIdleTasks () {
     var each;
 
@@ -155,14 +288,28 @@
         
       }
     }
+
+    return true;
   };
 
+  /**
+   * Called when a Timeywimey thinks it is about to execute a long-running task.
+   * Should be overridden by specific implementaions, in order to provide custom
+   * feedback to the user; e.g. loading widgets, and the like.
+   * @return {Object} Logs to the console by default.
+   */
   TimeyWimey.prototype.working = function working () {
-    console.log('Handling a potentially long-running task...');
+    return console.log('Handling a potentially long-running task...');
   };
 
+  /**
+   * Called when Timeywimey finishes what it thinks was a long-running task.
+   * Should be overridden to match the working() method, providing custom
+   * feedback to the user that the task is now complete.
+   * @return {Object} Logs to the console by default.
+   */
   TimeyWimey.prototype.finished = function finished () {
-    console.log('...Potentially long-running task complete.');
+    return console.log('...Potentially long-running task complete.');
   };
 
   root.TW = new TimeyWimey();
